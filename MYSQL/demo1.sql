3510fd92c1b47f5ea240318e299cd4af75c46177
RMBarbado <98925418+RMBarbado@users.noreply.github.com>
2023-03-21 12:26:45 +0800

    Add files via upload

---
+++ MYSQL/demo1.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE database;
+CREATE TABLE orderstable(OrderID INT NOT NULL, OrderNumber INT NOT NULL, PRIMARY KEY (OrderID));
+INSERT INTO orderstable(OrderId,OrderNumber) VALUES('1','77895'),('2','44678'),('3','22456');
